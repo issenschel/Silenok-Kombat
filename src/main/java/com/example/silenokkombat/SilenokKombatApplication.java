f72f8616ddd5bfffed0c7e874cebd3e4cc96ad24
issenschel <luckystqx@gmail.com>
2024-11-23 16:22:43 +0300

    Проект

---
+++ src/main/java/com/example/silenokkombat/SilenokKombatApplication.java
@@ -0,0 +1,13 @@
+package com.example.silenokkombat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilenokKombatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilenokKombatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/silenokkombat/SilenokKombatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.silenokkombat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilenokKombatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
